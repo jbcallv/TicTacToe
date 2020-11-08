f3a56704e4dd73f7a6897454936508e8bb1d01d0
jbcallv <jbcallv@gmail.com>
2020-11-08 16:42:26 -0500

    init

---
+++ Game.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+# dimensions of game window
+WIDTH = 350
+HEIGHT = 350
+size = (WIDTH, HEIGHT)
+
+# initialize window
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Tic-Tac-Toe")
+
+# create game window
+pygame.display.flip()
+
+# game loop
+running = True
+while (running):
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            running = False
