8f2509825df7c4322e108b08c485cab2bc5f04b4
jbcallv <jbcallv@gmail.com>
2020-12-09 15:04:39 -0500

    Made random computer opponent.

--- ComputerOpponent.py
+++ ComputerOpponent.py
@@ -1,11 +1,106 @@
 import pygame
+import random
 
 class ComputerOpponent:
     def __init__(self, window, grid, color):
         self.window = window
         self.grid = grid
         self.color = color
-        self.generateSmartMove()
+        #self.generateSmartMove()
 
-    def generateSmartMove(self):
-        pass
+    def generateSmartMove(self, taken_cells):
+        ind1 = random.randint(0, 2)
+        ind2 = random.randint(0, 2)
+        print(taken_cells)
+
+        while (taken_cells[ind1][ind2] == 1):
+            print(taken_cells[ind1][ind2])
+            ind1 = random.randint(0, 2)
+            ind2 = random.randint(0, 2)
+
+        # continue generating new position
+        if (ind1 == 0 and ind2 == 0):
+            self.keySeven()
+        if (ind1 == 0 and ind2 == 1):
+            self.keyEight()
+        if (ind1 == 0 and ind2 == 2):
+            self.keyNine()
+        if (ind1 == 1 and ind2 == 0):
+            self.keyFour()
+        if (ind1 == 1 and ind2 == 1):
+            self.keyFive()
+        if (ind1 == 1 and ind2 == 2):
+            self.keySix()
+        if (ind1 == 2 and ind2 == 0):
+            self.keyOne()
+        if (ind1 == 2 and ind2 == 1):
+            self.keyTwo()
+        if (ind1 == 2 and ind2 == 2):
+            self.keyThree()
+
+
+    def keySeven(self):
+        """ draws an x in the upper left corner of the grid """
+        #self.position_taken.append(1)
+        if (self.grid.cells[0][0] == 0):
+            self.grid.cells[0][0] = 1
+            pygame.draw.line(self.window, self.color, (0, 0), (105, 105))
+            #pygame.draw.line(self.window, self.color, (0, 105), (105, 0))
+
+    def keyEight(self):
+        """ places x at row 0 column 1 of grid """
+        if (self.grid.cells[0][1] == 0):
+            self.grid.cells[0][1] = 1
+            pygame.draw.line(self.window, self.color, (108, 0), (240, 105))
+            #pygame.draw.line(self.window, self.color, (108, 105), (240, 0))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyNine(self):
+        """ places x at row 0 column 2 of grid """
+        if (self.grid.cells[0][2] == 0):
+            self.grid.cells[0][2] = 1
+            pygame.draw.line(self.window, self.color, (245, 0), (350, 105))
+            #pygame.draw.line(self.window, self.color, (245, 105), (350, 0))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyFour(self):
+        if (self.grid.cells[1][0] == 0):
+            self.grid.cells[1][0] = 1
+            pygame.draw.line(self.window, self.color, (0, 108), (105, 240))
+            #pygame.draw.line(self.window, self.color, (0, 240), (108, 105))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyFive(self):
+        if (self.grid.cells[1][1] == 0):
+            self.grid.cells[1][1] = 1
+            pygame.draw.line(self.window, self.color, (108, 108), (240, 240))
+            #pygame.draw.line(self.window, self.color, (108, 240), (240, 108))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keySix(self):
+        if (self.grid.cells[1][2] == 0):
+            self.grid.cells[1][2] = 1
+            pygame.draw.line(self.window, self.color, (245, 108), (350, 240))
+            #pygame.draw.line(self.window, self.color, (245, 240), (350, 108))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyOne(self):
+        if (self.grid.cells[2][0] == 0):
+            self.grid.cells[2][0] = 1
+            pygame.draw.line(self.window, self.color, (0, 245), (105, 350))
+            #pygame.draw.line(self.window, self.color, (0, 350), (105, 245))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyTwo(self):
+        if (self.grid.cells[2][1] == 0):
+            self.grid.cells[2][1] = 1
+            pygame.draw.line(self.window, self.color, (108, 245), (240, 350))
+            #pygame.draw.line(self.window, self.color, (108, 350), (240, 245))
+        #self.compOpp.generateSmartMove(self.cells)
+
+    def keyThree(self):
+        if (self.grid.cells[2][2] == 0):
+            self.grid.cells[2][2] = 1
+            pygame.draw.line(self.window, self.color, (240, 245), (350, 350))
+            #pygame.draw.line(self.window, self.color, (240, 350), (350, 245))
+        #self.compOpp.generateSmartMove(self.cells)
--- Grid.py
+++ Grid.py
@@ -17,17 +17,9 @@ class Grid:
 
         # uses row, column format to check if a 
         # cell in the grid has been used. 1=cell taken; 0=not taken
-        self.zeroZero = False
-        self.zeroOne = False
-        self.zeroTwo = False
-
-        self.oneZero = False
-        self.oneOne = False
-        self.oneTwo = False
-
-        self.twoZero = False
-        self.twoOne = False
-        self.twoTwo = False 
+        self.cells = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]]
 
         self.compOpp = ComputerOpponent(self.window, self, self.color)
 
@@ -48,57 +40,66 @@ class Grid:
     def keySeven(self):
         """ draws an x in the upper left corner of the grid """
         #self.position_taken.append(1)
-        if (self.zeroZero == False):
-            self.zeroZero = True
+        if (self.cells[0][0] == 0):
+            self.cells[0][0] = 1
             pygame.draw.line(self.window, self.color, (0, 0), (105, 105))
             pygame.draw.line(self.window, self.color, (0, 105), (105, 0))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyEight(self):
         """ places x at row 0 column 1 of grid """
-        if (self.zeroOne == False):
-            self.zeroOne = True
+        if (self.cells[0][1] == 0):
+            self.cells[0][1] = 1
             pygame.draw.line(self.window, self.color, (108, 0), (240, 105))
             pygame.draw.line(self.window, self.color, (108, 105), (240, 0))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyNine(self):
         """ places x at row 0 column 2 of grid """
-        if (self.zeroTwo == False):
-            self.zeroTwo = True
+        if (self.cells[0][2] == 0):
+            self.cells[0][2] = 1
             pygame.draw.line(self.window, self.color, (245, 0), (350, 105))
             pygame.draw.line(self.window, self.color, (245, 105), (350, 0))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyFour(self):
-        if (self.oneZero == False):
-            self.oneZero = True
+        if (self.cells[1][0] == 0):
+            self.cells[1][0] = 1
             pygame.draw.line(self.window, self.color, (0, 108), (105, 240))
             pygame.draw.line(self.window, self.color, (0, 240), (108, 105))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyFive(self):
-        if (self.oneOne == False):
-            self.oneOne = True
+        if (self.cells[1][1] == 0):
+            self.cells[1][1] = 1
             pygame.draw.line(self.window, self.color, (108, 108), (240, 240))
             pygame.draw.line(self.window, self.color, (108, 240), (240, 108))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keySix(self):
-        if (self.oneTwo == False):
-            self.oneTwo = True
+        if (self.cells[1][2] == 0):
+            self.cells[1][2] = 1
             pygame.draw.line(self.window, self.color, (245, 108), (350, 240))
             pygame.draw.line(self.window, self.color, (245, 240), (350, 108))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyOne(self):
-        if (self.twoZero == False):
-            self.twoZero = True
+        if (self.cells[2][0] == 0):
+            self.cells[2][0] = 1
             pygame.draw.line(self.window, self.color, (0, 245), (105, 350))
             pygame.draw.line(self.window, self.color, (0, 350), (105, 245))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyTwo(self):
-        if (self.twoOne == False):
-            self.twoOne = True
+        if (self.cells[2][1] == 0):
+            self.cells[2][1] = 1
             pygame.draw.line(self.window, self.color, (108, 245), (240, 350))
             pygame.draw.line(self.window, self.color, (108, 350), (240, 245))
+        self.compOpp.generateSmartMove(self.cells)
 
     def keyThree(self):
-        if (self.twoTwo == False):
-            self.twoTwo = True
+        if (self.cells[2][2] == 0):
+            self.cells[2][2] = 1
             pygame.draw.line(self.window, self.color, (240, 245), (350, 350))
             pygame.draw.line(self.window, self.color, (240, 350), (350, 245))
+        self.compOpp.generateSmartMove(self.cells)
